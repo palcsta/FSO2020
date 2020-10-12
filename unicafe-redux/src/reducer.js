128253f5ffff3e438db2fbca31e6509596e1a1aa
Stanislav Palchyk <stanislav.palchyk@helsinki.fi>
2020-10-12 20:22:12 +0300

    osa 6.1-2

---
+++ unicafe-redux/src/reducer.js
@@ -0,0 +1,37 @@
+let initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  
+  console.log(action)
+  
+  // initialState.good++;
+  // state.good++  ;
+  console.log('states ::: ', initialState)
+  switch (action.type) {
+    case 'GOOD':
+      return state = {good: state.good+1,
+        ok: state.ok,
+        bad: state.bad, }
+    case 'OK':
+      return state = {good: state.good,
+        ok: state.ok+1,
+        bad: state.bad, }
+    case 'BAD':
+      return state = {good: state.good,
+        ok: state.ok,
+        bad: state.bad+1, }
+    case 'ZERO':
+      return state = {good: 0,
+        ok: 0,
+        bad: 0, }
+      
+    default: return state
+  }
+
+}
+
+export default counterReducer
